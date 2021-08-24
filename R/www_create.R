18d5d5e61fcc5e49d208fc14c5e41d05780a9502
Abbey Thomas <thomasabbey8@gmail.com>
2021-08-24 12:14:45 -0500

    Added ReadMe, updated description, added preliminary www_create() func.

--- R/www_create.R
+++ R/www_create.R
@@ -1,22 +1,22 @@
 #' Create the 'www' Directory for a Shiny App
 #'
+#' @description Many shiny apps for speech experiments require resources (like WAV files) in the www subdirectory. This command takes care of gathering datafiles and putting them in the appropriate directory for
 #' @param from A character vector of filenames or a single directory name wrapped in c(). Leave `NULL` (default) if you only want to add one of the datasets included in the package.
 #' @param is_dir Boolean. Is 'from' a directory?
 #' @param path A character vector naming the path to the parent directory of 'www'. Defaults to working directory.
-#' @param volumeCalibration Boolean. Should `speechcollectr::cal_noise()` for testing headphone volume be copied to 'www'?
-#' @param HugginsPitchScreen Boolean. Should `speechcollectr::HugginsPitchData()` for Huggins Pitch Headphone Screen be copied to 'www'?
-#' @param AntiphaseScreen Boolean. Should `speechcollectr::AntiphaseData()` for Antiphase Headphone Screen be copied to 'www'?
+#' @param volumeCalibration Boolean. Should `cal_noise()` for testing headphone volume be copied to 'www'?
+#' @param HugginsPitchScreen Boolean. Should `HugginsPitchData()` for Huggins Pitch Headphone Screen be copied to 'www'?
+#' @param AntiphaseScreen Boolean. Should `AntiphaseData()` for Antiphase Headphone Screen be copied to 'www'?
 #'
-#' @return Success messages will be printed in the console as each step towards creating the directory is completed.
+#' @return A www subdirectory in the current working directory. Success messages will be printed in the console as each step towards creating the directory is completed.
 #' @export
 #'
 #' @examples
 #'
 # TO DO:
-# 1. Add recorder scripts.
+
 # 2. Add game functions.
 # 3. Add examples.
-# 4. Change console output to give filenames.
 
 www_create <- function(from = NULL, is_dir = FALSE, path = ".",
                        volumeCalibration = FALSE,
@@ -53,7 +53,7 @@ www_create <- function(from = NULL, is_dir = FALSE, path = ".",
   if (isTRUE(volumeCalibration)) {
     vol_cal <- data("cal_noise")
     writeWave(vol_cal, "www/cal_noise.wave")
-    cat("Success: Volume calibration noise WAV added to 'www'!")
+    cat("Success: Volume calibration noise WAV added to 'www' as 'cal_noise.wav'.")
   }
 
   if (isTRUE(HugginsPitchScreen)) {
@@ -61,7 +61,7 @@ www_create <- function(from = NULL, is_dir = FALSE, path = ".",
     for (i in 1:14) {
       tuneR::writeWave(hp_data[[i]], filename = paste0("www/", names(hp_data[i])))
     }
-    cat("Success: Huggins-Pitch Headphone Screen data added to 'www'!")
+    cat("Success: Huggins-Pitch Headphone Screen data added to 'www'! All Huggins Pitch files (12) are prefixed with 'hp_'.")
   }
 
   if (isTRUE(AntiphaseScreen)) {
@@ -69,7 +69,7 @@ www_create <- function(from = NULL, is_dir = FALSE, path = ".",
     for (i in 1:7) {
       tuneR::writeWave(ap_data[[i]], filename = paste0("www/", names(ap_data[i])))
     }
-    cat("Success: Antiphase Headphone Screen data added to 'www'!")
+    cat("Success: Antiphase Headphone Screen data added to 'www'! All antiphase screen files (6) are prefixed with 'ap_'.")
   }
 
 }
---
+++ README.md
@@ -0,0 +1,41 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# speechcollectr
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+NOTE (August 2021): This package is currently under development. New
+functions in speechcollectr are being added daily\! Stay tuned for more
+R tools for speech collection.
+
+The goal of speechcollectr is to provide tools for speech production and
+perception experiments. In the context of the COVID-19 pandemic, many
+speech researchers have had to either stop data collection or move to
+web-based data collection. This generally requires researchers to
+familiarize themselves with a new software. Speechcollectr, however,
+provides tools for web-based data collection in R, a language that many
+speech scientists are already familiar with.
+
+In its full version, speechcollectr will include R shiny buttons for
+audio playback, WAV file audio recording capabilities, simple games for
+participants to play as data is collected, and tools for acoustic
+analysis to present feedback to participants.
+
+You will find a working demonstration of a speechcollectr production
+task at <https://abbeythomas.shinyapps.io/new-recorder/>, and
+demonstrations of the headphone screening tests at
+<https://abbeythomas.shinyapps.io/huggins-pitch-screen/> and
+<https://abbeythomas.shinyapps.io/antiphase-screen/>.
+
+## Installation
+
+This project is not yet available on CRAN. For now, you can install the
+development version from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("abbey-thomas/speechcollectr")
+```
